996263a00cbab7e53fc95521365c3e9efa2f939c
cannatx <nachocane@msn.com>
2022-02-25 18:58:48 +0000

    uodate on master

--- python_git_test.py
+++ python_git_test.py
@@ -5,27 +5,28 @@ br_name = "master"
 repo = git.Repo("./")
 
 # branches = repo.branches
-# remotes = repo.remotes
-# origin = remotes["origin"]
-# Create Local/ remote
+try:
+    repo.git.checkout(br_name)
+except:
+    repo.git.fetch()
+    remotes = [r.name for r in repo.remotes.origin.refs]
+    if f"origin/{br_name}" not in remotes:
+        repo.git.checkout("-b", br_name)
+        repo.git.push("--set-upstream", "origin", br_name)
 
+    branches = [b.name for b in repo.branches]
+    if br_name not in branches:
+        repo.git.checkout("-b", br_name, f"origin/{br_name}")
 
-# remote_refs = list(repo.remotes['origin'].refs)
-remotes = [r.name for r in repo.remotes.origin.refs]
-if f"origin/{br_name}" not in remotes:
-    repo.git.checkout("-b", br_name)
-    repo.git.push("--set-upstream", "origin", br_name)
+    repo.git.checkout(br_name)
 
-
-branches = [b.name for b in repo.branches]
-if br_name not in branches:
-    repo.git.checkout("-b", br_name, f"origin/{br_name}")
-
-
-# repo.git.checkout(br_name)
-
-repo.git.add("-A")
-
-repo.git.commit("-m update")
-
-repo.remotes.origin.push()
+try:
+    repo.git.add("-A")
+    repo.git.commit(f"-m uodate on {br_name}")
+    # repo.remotes.origin.push()
+    repo.git.push("origin", br_name)
+except Exception as e:
+    if "nothing to commit" in e:
+        pass
+    else:
+        print(e)
