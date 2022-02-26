48a4e70ff9636f9d2649dc618bbd3512ee8251eb
cannatx <nachocane@msn.com>
2022-02-26 07:04:14 +0000

    update on A_branch

--- python_git_test.py
+++ python_git_test.py
@@ -2,7 +2,7 @@ import git
 
 br_name = "A_branch"
 
-repo = git.Repo("./")
+repo = git.Repo()
 
 # branches = repo.branches
 try:
@@ -22,7 +22,7 @@ except:
 
 try:
     repo.git.add("-A")
-    repo.git.commit(f"-m uodate on {br_name}")
+    repo.git.commit(f"-m update on {br_name}")
     # repo.remotes.origin.push()
     repo.git.push("origin", br_name)
 except Exception as e:
